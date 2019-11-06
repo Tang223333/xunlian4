56f30053d9ebd9dde4896a9d19fe39fc4cf6a015
admin <1755517380@qq.com>
2019-11-06 21:21:55 +0800

    唐海龙的第一个页面

--- app/src/main/java/com/lenovo/manufacture/Main2Activity.java
+++ app/src/main/java/com/lenovo/manufacture/Main2Activity.java
@@ -1,14 +1,27 @@
 package com.lenovo.manufacture;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Main2Activity extends AppCompatActivity {
+import com.lenovo.manufacture.thl.TangHaiLong_01;
+
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        TextView thl=findViewById(R.id.thl);
+        thl.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent=new Intent(Main2Activity.this, TangHaiLong_01.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/lenovo/manufacture/thl/TangHaiLong_01.java
@@ -0,0 +1,16 @@
+package com.lenovo.manufacture.thl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lenovo.manufacture.R;
+
+public class TangHaiLong_01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tang_hai_long_01);
+    }
+}
