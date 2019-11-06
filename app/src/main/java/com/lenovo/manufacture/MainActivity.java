43a994b7b32f5b263a142f88fef28282f95aed77
mc <2695734816@qq.com>
2019-11-06 20:03:00 +0800

    创建了个人跳转的跳板

--- app/src/main/java/com/lenovo/manufacture/MainActivity.java
+++ app/src/main/java/com/lenovo/manufacture/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.FrameLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 /**
  * @author Amoly
  * @date 2019/10/24.
@@ -23,6 +24,8 @@ import androidx.appcompat.app.AppCompatActivity;
  */
 public class MainActivity extends AppCompatActivity {
 
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,26 +58,51 @@ public class MainActivity extends AppCompatActivity {
                 /* mWebView.showLog("test Log"); */
             }
         });
-
+        //TODO 启用Java脚本
         webView.getSettings().setJavaScriptEnabled(true);
+        //TODO 设置Java脚本可以自动打开窗口
         webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
         webView.setBackgroundColor(0);
+        //TODO 请求焦点
         webView.requestFocus();
+        //TODO 添加Javascript接口
+        /**
+         * TODO 只有声明了@JavascriptInterace注解的方法才能被Web页面调用
+         *
+         * 为了与Web页面实现动态交互，Android应用程序允许WebView通过WebView.addJavascriptInterface接口向Web页面注入Java对象，
+         * 页面Javascript脚本可直接引用该对象并调用该对象的方法。
+         * 这类应用程序一般都会有类似如下的代码：
+         *      webView.addJavascriptInterface(javaObj, "jsObj");
+         * 此段代码将javaObj对象暴露给js脚本，可以通过jsObj对象对其进行引用，调用javaObj的方法。
+         * 结合Java的反射机制可以通过js脚本执行任意Java代码。
+         *
+         */
+        //nativeMethod是js调用原生时的别名，非固定写法
         webView.addJavascriptInterface(new JavaScriptInterface(this), "nativeMethod");
         WebSettings webSetting = webView.getSettings();
+        //TODO 设置允许文件访问
         webSetting.setAllowFileAccess(true);
+        //TODO 设置布局算法
         webSetting.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NORMAL);
+        //TODO 设置支持缩放
         webSetting.setSupportZoom(true);
+        //TODO 设置内建在缩放控制
         webSetting.setBuiltInZoomControls(true);
+        //TODO 设置使用宽的视图端口
         webSetting.setUseWideViewPort(true);
+        //TODO 支持多个窗口
         webSetting.setSupportMultipleWindows(false);
+        //TODO 设置App缓存启用
         webSetting.setAppCacheEnabled(true);
+        //TODO 启用设置Dom存储
         webSetting.setDomStorageEnabled(true);
+        //TODO 设置默认的文本编码名称
         webSetting.setDefaultTextEncodingName("utf-8");
+        //TODO 设置应用程序缓存的最大大小
         webSetting.setAppCacheMaxSize(Long.MAX_VALUE);
-
-
+        //TODO 得到资源.得到显示指标
         int screenDensity = getResources().getDisplayMetrics().densityDpi;
+        //TODO 变焦密度
         WebSettings.ZoomDensity zoomDensity = WebSettings.ZoomDensity.MEDIUM;
         switch (screenDensity) {
             case DisplayMetrics.DENSITY_LOW:
@@ -87,9 +115,9 @@ public class MainActivity extends AppCompatActivity {
                 zoomDensity = WebSettings.ZoomDensity.FAR;
                 break;
         }
+        //TODO 设置默认缩放
         webSetting.setDefaultZoom(zoomDensity);
     }
-
 
 
     public class JavaScriptInterface {
@@ -103,12 +131,74 @@ public class MainActivity extends AppCompatActivity {
          * 与js交互时用到的方法，在js里直接调用的
          */
         @JavascriptInterface
-        public void startActivity() {
-            Intent intent = new Intent();
+        public void startActivity(int id) {
+            switch (id) {
+                case 1:
+                    Intent intent1 = new Intent();
 //            intent.putExtra("fromWhich", "webViewUrl");
 //            intent.putExtra("replyID", "replyID");
-            intent.setClass(mActivity, TestActivity.class);
-            mActivity.startActivity(intent);
+//                    intent1.setClass(mActivity, TestActivity.class);
+                    intent1.setClass(mActivity, Main1Activity.class);
+                    mActivity.startActivity(intent1);
+                    break;
+                case 2:
+                    Intent intent2 = new Intent();
+//            intent.putExtra("fromWhich", "webViewUrl");
+//            intent.putExtra("replyID", "replyID");
+                    intent2.setClass(mActivity, Main2Activity.class);
+                    mActivity.startActivity(intent2);
+                    break;
+                case 3:
+                    Intent intent3 = new Intent();
+//            intent.putExtra("fromWhich", "webViewUrl");
+//            intent.putExtra("replyID", "replyID");
+                    intent3.setClass(mActivity, Main3Activity.class);
+                    mActivity.startActivity(intent3);
+                    break;
+                case 4:
+                    Intent intent4 = new Intent();
+//            intent.putExtra("fromWhich", "webViewUrl");
+//            intent.putExtra("replyID", "replyID");
+                    intent4.setClass(mActivity, Main4Activity.class);
+                    mActivity.startActivity(intent4);
+                    break;
+                case 5:
+                    Intent intent5 = new Intent();
+//            intent.putExtra("fromWhich", "webViewUrl");
+//            intent.putExtra("replyID", "replyID");
+                    intent5.setClass(mActivity, Main5Activity.class);
+                    mActivity.startActivity(intent5);
+                    break;
+                case 6:
+                    Intent intent6 = new Intent();
+//            intent.putExtra("fromWhich", "webViewUrl");
+//            intent.putExtra("replyID", "replyID");
+                    intent6.setClass(mActivity, Main6Activity.class);
+                    mActivity.startActivity(intent6);
+                    break;
+                case 7:
+                    Intent intent7 = new Intent();
+//            intent.putExtra("fromWhich", "webViewUrl");
+//            intent.putExtra("replyID", "replyID");
+                    intent7.setClass(mActivity, Main7Activity.class);
+                    mActivity.startActivity(intent7);
+                    break;
+                case 8:
+                    Intent intent8 = new Intent();
+//            intent.putExtra("fromWhich", "webViewUrl");
+//            intent.putExtra("replyID", "replyID");
+                    intent8.setClass(mActivity, Main8Activity.class);
+                    mActivity.startActivity(intent8);
+                    break;
+
+            }
+
+
+//            Intent intent = new Intent();
+////            intent.putExtra("fromWhich", "webViewUrl");
+////            intent.putExtra("replyID", "replyID");
+//            intent.setClass(mActivity, TestActivity.class);
+//            mActivity.startActivity(intent);
         }
 
     }
