0a6be0227855f1da9d3fd9e3248eaeef1c0e1e45
Dominator <1325871848@qq.com>
2019-11-07 17:15:02 +0800

    just for training

---
+++ app/src/main/java/com/lenovo/manufacture/dominator/MainActivity.java
@@ -0,0 +1,17 @@
+package com.lenovo.manufacture.dominator;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.lenovo.manufacture.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+    }
+}
