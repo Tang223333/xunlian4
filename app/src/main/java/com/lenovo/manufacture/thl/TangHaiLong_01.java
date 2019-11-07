b7995281be26b398ca6181472cce6c3cbf52ddea
admin <1755517380@qq.com>
2019-11-07 16:20:43 +0800

    唐海龙的ViewPager

--- app/src/main/java/com/lenovo/manufacture/thl/TangHaiLong_01.java
+++ app/src/main/java/com/lenovo/manufacture/thl/TangHaiLong_01.java
@@ -1,16 +1,51 @@
 package com.lenovo.manufacture.thl;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
 import com.lenovo.manufacture.R;
+import com.lenovo.manufacture.thl.frament.ThlFragment01Fragment;
+import com.lenovo.manufacture.thl.frament.ThlFragment02Fragment;
+import com.lenovo.manufacture.thl.frament.ThlFragment03Fragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TangHaiLong_01 extends AppCompatActivity {
+
+    private ViewPager thlViewpager;
+    List<Fragment> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tang_hai_long_01);
+        initView();
+    }
+
+    private void initView() {
+        thlViewpager = (ViewPager) findViewById(R.id.thl_viewpager);
+        list=new ArrayList<>();
+        list.add(new ThlFragment01Fragment());
+        list.add(new ThlFragment02Fragment());
+        list.add(new ThlFragment03Fragment());
+        FragmentPagerAdapter fragmentPagerAdapter=new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @NonNull
+            @Override
+            public Fragment getItem(int position) {
+                return list.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return list.size();
+            }
+        };
+        thlViewpager.setAdapter(fragmentPagerAdapter);
     }
 }
---
+++ app/src/main/java/com/lenovo/manufacture/thl/frament/ThlFragment03Fragment.java
@@ -0,0 +1,28 @@
+package com.lenovo.manufacture.thl.frament;
+
+import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import com.lenovo.manufacture.R;
+
+
+public class ThlFragment03Fragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.thl_fragment_03, null);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+}
