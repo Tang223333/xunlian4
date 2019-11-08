d983775f2e56499aac918596764d636b70cacd12
admin <1755517380@qq.com>
2019-11-08 13:50:37 +0800

    唐海龙

--- app/src/main/java/com/lenovo/manufacture/thl/TangHaiLong_01.java
+++ app/src/main/java/com/lenovo/manufacture/thl/TangHaiLong_01.java
@@ -1,6 +1,8 @@
 package com.lenovo.manufacture.thl;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,10 +18,13 @@ import com.lenovo.manufacture.thl.frament.ThlFragment03Fragment;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TangHaiLong_01 extends AppCompatActivity {
+public class TangHaiLong_01 extends AppCompatActivity implements View.OnClickListener {
 
     private ViewPager thlViewpager;
     List<Fragment> list;
+    private TextView One;
+    private TextView Two;
+    private TextView Three;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +35,11 @@ public class TangHaiLong_01 extends AppCompatActivity {
 
     private void initView() {
         thlViewpager = (ViewPager) findViewById(R.id.thl_viewpager);
-        list=new ArrayList<>();
+        list = new ArrayList<>();
         list.add(new ThlFragment01Fragment());
         list.add(new ThlFragment02Fragment());
         list.add(new ThlFragment03Fragment());
-        FragmentPagerAdapter fragmentPagerAdapter=new FragmentPagerAdapter(getSupportFragmentManager()) {
+        FragmentPagerAdapter fragmentPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
             @NonNull
             @Override
             public Fragment getItem(int position) {
@@ -47,5 +52,72 @@ public class TangHaiLong_01 extends AppCompatActivity {
             }
         };
         thlViewpager.setAdapter(fragmentPagerAdapter);
+        One = (TextView) findViewById(R.id.One);
+        One.setOnClickListener(this);
+        Two = (TextView) findViewById(R.id.Two);
+        Two.setOnClickListener(this);
+        Three = (TextView) findViewById(R.id.Three);
+        Three.setOnClickListener(this);
+        thlViewpager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position){
+                    case 0:
+                        csh();
+                        One.setSelected(true);
+                        thlViewpager.setCurrentItem(0);
+                        break;
+                    case 1:
+                        csh();
+                        Two.setSelected(true);
+                        thlViewpager.setCurrentItem(1);
+                        break;
+                    case 2:
+                        csh();
+                        Three.setSelected(true);
+                        thlViewpager.setCurrentItem(2);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        csh();
+        One.setSelected(true);
+    }
+
+    private void csh(){
+        One.setSelected(false);
+        Two.setSelected(false);
+        Three.setSelected(false);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.One:
+                csh();
+                One.setSelected(true);
+                thlViewpager.setCurrentItem(0);
+                break;
+            case R.id.Two:
+                csh();
+                Two.setSelected(true);
+                thlViewpager.setCurrentItem(1);
+                break;
+            case R.id.Three:
+                csh();
+                Three.setSelected(true);
+                thlViewpager.setCurrentItem(2);
+                break;
+        }
     }
 }
