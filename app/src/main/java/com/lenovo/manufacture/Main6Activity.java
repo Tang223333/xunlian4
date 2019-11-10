5a3fbfbbd3583a08199ed89696522d67f13eee7c
Dominator <1325871848@qq.com>
2019-11-10 14:59:51 +0800

    just for showdemo

--- app/src/main/java/com/lenovo/manufacture/Main6Activity.java
+++ app/src/main/java/com/lenovo/manufacture/Main6Activity.java
@@ -1,12 +1,27 @@
 package com.lenovo.manufacture;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.lenovo.manufacture.dominator.MessageActivity;
+
+import java.util.Locale;
+
+import static com.lenovo.manufacture.R.layout.dominator_view_dialog;
+
 public class Main6Activity extends AppCompatActivity {
+
+    AlertDialog alert=null;
+    AlertDialog.Builder builder = null;
+    View mydialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,4 +31,52 @@ public class Main6Activity extends AppCompatActivity {
     }
 
 
+    public void btn_selectdialog(View view) {
+
+        builder = new AlertDialog.Builder(Main6Activity.this);
+
+        //加载自定义的那个View,同时设置下
+        final LayoutInflater inflater = Main6Activity.this.getLayoutInflater();
+      mydialog = inflater.inflate(dominator_view_dialog, null,false);
+        builder.setView(mydialog);
+        builder.setCancelable(false);
+        alert = builder.create();
+
+        alert.show();
+
+        EditText wherego=mydialog.findViewById(R.id.dominator_place);
+
+        mydialog.findViewById(R.id.dominator_go).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              if(wherego.getText().toString().equals("message")){
+                  Intent intent=new Intent(Main6Activity.this,MessageActivity.class);
+                  startActivity(intent);
+              }
+
+
+
+
+            }
+        });
+
+        mydialog.findViewById(R.id.dominator_cancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              alert.dismiss();
+
+
+
+
+            }
+        });
+
+
+
+
+
+
+
+
+    }
 }
---
+++ app/src/main/java/com/lenovo/manufacture/dominator/MessageActivity.java
@@ -0,0 +1,16 @@
+package com.lenovo.manufacture.dominator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lenovo.manufacture.R;
+
+public class MessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dominator_activity_message);
+    }
+}
