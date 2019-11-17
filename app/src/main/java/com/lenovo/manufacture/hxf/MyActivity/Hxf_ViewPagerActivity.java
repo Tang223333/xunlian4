4a1059ebdf0813d0596dcf6ed9695f236ac9b364
mc <2695734816@qq.com>
2019-11-17 21:00:05 +0800

    hxf-新增了：优化了页面布局，实现了播放上一个视频、下一个视频的功能

--- app/src/main/java/com/lenovo/manufacture/hxf/MyActivity/Hxf_ViewPagerActivity.java
+++ app/src/main/java/com/lenovo/manufacture/hxf/MyActivity/Hxf_ViewPagerActivity.java
@@ -10,7 +10,7 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.WindowManager;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -29,6 +29,7 @@ import com.lenovo.manufacture.BuildConfig;
 import com.lenovo.manufacture.R;
 import com.lenovo.manufacture.hxf.Utils.CustomVideoView;
 import com.lenovo.manufacture.hxf.Utils.GetPhoneInfo;
+import com.lenovo.manufacture.hxf.Utils.log;
 import com.lenovo.manufacture.hxf.adapter.MyViewPagerAdapter;
 
 import java.util.LinkedList;
@@ -37,8 +38,9 @@ import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class Hxf_ViewPagerActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener, View.OnClickListener, MediaPlayer.OnCompletionListener {
+public class Hxf_ViewPagerActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener, View.OnClickListener, MediaPlayer.OnCompletionListener, MediaPlayer.OnPreparedListener {
 
+    private boolean STATE = true;
     private ViewPager viewPager;
     private TextView phone_Info;
     private Timer timer;
@@ -46,6 +48,8 @@ public class Hxf_ViewPagerActivity extends AppCompatActivity implements ViewPage
     private Button mBtnStopFlip;
     private Button mBtnPlayMusic;
     private Button mBtnPlayVideo;
+    private Button mBtnPlayBeforeVideo;
+    private Button mBtnPlayAfterVideo;
     private Dialog dialog;
     private ImageView mIvDialogIcon;
     private TextView mTvDialogTitle;
@@ -54,12 +58,14 @@ public class Hxf_ViewPagerActivity extends AppCompatActivity implements ViewPage
     private Button mBtnDialogNeutral;
     private Button mBtnDialogPositive;
     private int currentItem; //TODO 初始化游标(当前展示的viewPager的序号)
+    private int currentVideo;//TODO 初始化音乐播放的游标
     private Handler handler = new Handler();
     private VideoView videoView;
     private List<View> viewList;
     private List<String> stringList;
     private LinearLayout layout_viewPager_father;
     private MediaPlayer mediaPlayer;
+    private LinkedList<Integer> videoResource;
     private PagerTabStrip mViewPagerTab;
     private PagerTitleStrip mViewPagerTitle;
     private TextView tv_viewPager01;
@@ -68,6 +74,8 @@ public class Hxf_ViewPagerActivity extends AppCompatActivity implements ViewPage
     private List<View> tabViewList;
     private LinearLayout layout_topTab;
     private FrameLayout layout_dialogVideo_father;
+    private String resource = String.valueOf(R.raw.pm);
+    private MediaController mediaController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -169,12 +177,32 @@ public class Hxf_ViewPagerActivity extends AppCompatActivity implements ViewPage
         mBtnStopFlip.setOnClickListener(this);
         mBtnPlayVideo = (Button) findViewById(R.id.btn_playVideo);
         mBtnPlayVideo.setOnClickListener(this);
+
+        mBtnPlayBeforeVideo = (Button) findViewById(R.id.btn_playBeforeVideo);
+        mBtnPlayBeforeVideo.setOnClickListener(this);
+        mBtnPlayAfterVideo = (Button) findViewById(R.id.btn_playAfterVideo);
+        mBtnPlayAfterVideo.setOnClickListener(this);
+
         layout_viewPager_father = findViewById(R.id.layout_viewPager_father);
         layout_viewPager_father.setBackgroundColor(Color.rgb(0, 0, 0));
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.ge);
-        videoView = new VideoView(this);
+        videoView = new CustomVideoView(this);
+//        videoView = new VideoView(this);
+        //TODO 设置播放结束的监听器
+        videoView.setOnCompletionListener(this);
+        //TODO 在准备好的侦听器上设置
+        videoView.setOnPreparedListener(this);
+        videoResource = new LinkedList<>();
+        videoResource.add(R.raw.pm3);
+        videoResource.add(R.raw.pm);
+        videoResource.add(R.raw.pm1);
+        videoResource.add(R.raw.pm2);
+        videoResource.add(R.raw.pm3);
+        videoResource.add(R.raw.pm2);
         View inflate = LayoutInflater.from(this).inflate(R.layout.hxf_custom_dialog, null, false);
         customDialogShow(inflate, R.mipmap.icon, getString(R.string.dialog_title), getString(R.string.dialog_content));
+        //TODO 创建媒体控制器
+        mediaController = new MediaController(this);
     }
 
     private void initDialog(View view) {
@@ -193,7 +221,6 @@ public class Hxf_ViewPagerActivity extends AppCompatActivity implements ViewPage
         initDialog(layout);
         if (dialog_icon != 0) {
             mIvDialogIcon.setBackgroundResource(dialog_icon);
-
         }
         if (!dialog_title.isEmpty()) {
             mTvDialogTitle.setText(dialog_title);
@@ -257,7 +284,7 @@ public class Hxf_ViewPagerActivity extends AppCompatActivity implements ViewPage
                 break;
             case R.id.btn_startFlip:
                 timer.cancel();
-                removeAllView();
+                removeAllViews();
                 layout_viewPager_father.addView(layout_topTab);
                 layout_viewPager_father.addView(viewPager);
                 initViewPager();
@@ -267,49 +294,80 @@ public class Hxf_ViewPagerActivity extends AppCompatActivity implements ViewPage
                 timer.cancel();
                 break;
             case R.id.btn_playVideo:
+                try {
 //                layout_viewPager_father.setPadding(45, 60, 45, 60);
-                removeAllView();
-                videoView = new CustomVideoView(this);
-                videoView.setOnCompletionListener(this);
+                    removeAllViews();
 //                layout_viewPager_father.addView(videoView);
 
-                //TODO 对话框式视频播放器
-                View inflate = LayoutInflater.from(this).inflate(R.layout.hxf_blank_dialog, null, false);
-                customDialogShow(inflate,0,"","");
-                layout_dialogVideo_father = inflate.findViewById(R.id.layout_dialogVideo_father);
-                layout_dialogVideo_father.removeAllViews();
-                layout_dialogVideo_father.addView(videoView);
-
-                //TODO 设置视频文件
-                videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.pm));
-                //TODO 设置控制条
-                videoView.setMediaController(new MediaController(this));
-                videoView.start();
+                    //TODO 对话框式视频播放器
+                    View inflate = LayoutInflater.from(this).inflate(R.layout.hxf_blank_dialog, null, false);
+                    customDialogShow(inflate, 0, "", "");
+                    layout_dialogVideo_father = inflate.findViewById(R.id.layout_dialogVideo_father);
+                    layout_dialogVideo_father.addView(videoView);
+                    afterVideoResource();
+                    setVideoResource();
+                    STATE = false;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    log.d("", e.toString());
+                }
                 break;
             case R.id.btn_dialog_negative:
                 dialog.dismiss();
                 if (layout_dialogVideo_father != null) {
-                    rePlayVideo();
+                    initPlayVideo();
                 }
                 Toast.makeText(this, "已取消！", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn_dialog_neutral:
                 dialog.dismiss();
                 if (layout_dialogVideo_father != null) {
-                    rePlayVideo();
+                    initPlayVideo();
                 }
                 Toast.makeText(this, "已忽略！", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn_dialog_positive:
                 dialog.dismiss();
                 if (layout_dialogVideo_father != null) {
-                    rePlayVideo();
+                    initPlayVideo();
                 }
                 Toast.makeText(this, "已确定！", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_playBeforeVideo:
+                if (STATE){
+                    Toast.makeText(this, "请先播放弹窗视频！", Toast.LENGTH_SHORT).show();
+                }else {
+                    initPlayVideo();
+                    beforeVideoResource();
+                }
+                break;
+            case R.id.btn_playAfterVideo:
+                if (STATE){
+                    Toast.makeText(this, "请先播放弹窗视频！", Toast.LENGTH_SHORT).show();
+                }else {
+                    initPlayVideo();
+                    afterVideoResource();
+                }
                 break;
             default:
                 break;
         }
+    }
+
+    private void beforeVideoResource() {
+        //TODO 上一个视频
+        currentVideo = (Math.abs((currentVideo - 1) % videoResource.size()));
+        log.d("", String.valueOf(currentVideo));
+        resource = String.valueOf(videoResource.get(currentVideo));
+        videoView.start();
+    }
+
+    private void afterVideoResource() {
+        //TODO 下一个视频
+        currentVideo = (currentVideo + 1) % videoResource.size();
+        log.d("", String.valueOf(currentVideo));
+        resource = String.valueOf(videoResource.get(currentVideo));
+        videoView.start();
     }
 
     private void playMusic() {
@@ -324,29 +382,45 @@ public class Hxf_ViewPagerActivity extends AppCompatActivity implements ViewPage
         }
     }
 
-    private void removeAllView() {
+    private void removeAllViews() {
         layout_viewPager_father.removeAllViews();
 //        layout_viewPager_father.removeView(viewPager);
 //        layout_viewPager_father.removeView(videoView);
     }
 
+    //TODO 播放器已经准备ok时触发的监听器
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+
+    }
+
+    //TODO 播放完毕时执行的操作
     @Override
     public void onCompletion(MediaPlayer mp) {
         if (dialog.isShowing()) {
             dialog.dismiss();
-            rePlayVideo();
+            initPlayVideo();
         }
     }
 
-    private void rePlayVideo() {
-        removeAllView();
-        layout_dialogVideo_father.removeAllViews();
+    private void initPlayVideo() {
+        try {
+            removeAllViews();
 //        layout_viewPager_father.setPadding(45, 60, 45, 60);
-        layout_viewPager_father.addView(videoView);
+            layout_dialogVideo_father.removeAllViews();
+            layout_viewPager_father.addView(videoView);
+            setVideoResource();
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.d("", e.toString());
+        }
+    }
+
+    private void setVideoResource() {
         //TODO 设置视频文件
-        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.pm));
-        //TODO 设置控制条
-        videoView.setMediaController(new MediaController(this));
+        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + resource));
+        //TODO 设置媒体控制器
+        videoView.setMediaController(mediaController);
         videoView.start();
     }
 }
